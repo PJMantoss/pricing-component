7ec4497c8f6e13eb0a9c0d2246772827762aa68c
PJMantoss <joelptoss@gmail.com>
2023-09-05 23:49:51 +0100

    update style

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const body = document.querySelector("body"),
     toggle = document.querySelector(".toggleDiv");
 
+toggle.addEventListener("click", () => {
+    body.classList.toggle("dark")
+});
+
 toggle.addEventListener("click", () => toggle.classList.toggle("active"));
