3aec6cdde993c17c69386da59de4ef5bd94d4b25
PJMantoss <joelptoss@gmail.com>
2023-09-07 23:19:34 +0100

    update script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const container = document.querySelector(".container"),
     if(getMode && getMode === "dark"){
         container.classList.toggle("dark");
     }
-    console.log(getMode);
+    //console.log(getMode);
 
 toggle.addEventListener("click", () => {
     container.classList.toggle("dark");
