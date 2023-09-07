403501b878b08c59fc70d72a4a4371e89d7d1e00
PJMantoss <joelptoss@gmail.com>
2023-09-07 23:13:15 +0100

    update style

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const container = document.querySelector(".container"),
 
 toggle.addEventListener("click", () => {
     container.classList.toggle("dark");
-});
 
-if(!container.classList.toggle("dark")){}
+    if(!container.classList.contains("dark")){
+        return localStorage.setItem()
+    }
+});
 
 toggle.addEventListener("click", () => toggle.classList.toggle("active"));
