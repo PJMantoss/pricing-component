0194420df33f42c1a0ada2ea677e5879645a6278
PJMantoss <joelptoss@gmail.com>
2023-09-21 22:58:32 +0100

    update script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container.active");
 let toggle = document.querySelector(".toggleDiv");
 
     // let getMode = localStorage.getItem("mode");
